935e1154ce42be1c8f5a0268cee9056134c31913
dennisvoo <denn.is.vooo@gmail.com>
2019-08-26 21:38:16 -0700

    typo in error message

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ MAX_DESCRIPTION_LENGTH = 140
 
 # ERR MSGS
 CORRECT_TITLE_FORMAT = "{game}@{hour:minutes}"
-TITLE_FORMAT_INCORRECT = "Incorrect tile format. ```correct format: {}```".format(CORRECT_TITLE_FORMAT)
+TITLE_FORMAT_INCORRECT = "Incorrect title format. ```correct format: {}```".format(CORRECT_TITLE_FORMAT)
 DESCR_TOO_LONG = "Max description length is " + str(MAX_DESCRIPTION_LENGTH)
 LOBBY_ALREADY_EXISTS = "Lobby already exists."
 
