e700cc2b3bc1e9f8a81d92a5136a2a48483ed277
dennisvoo <denn.is.vooo@gmail.com>
2019-08-27 23:35:36 -0700

    fixed bug where users couldnt react to other messages

--- utility.py
+++ utility.py
@@ -10,12 +10,12 @@ async def add_checkin(message,lobbies):
             curr_lobby.has_emoji = True
 
 async def checkin_player(reaction,user,lobbies):
-    emoji  = reaction.emoji
-    if emoji != WHITE_HEAVY_CHECK_MARK:
-        await reaction.message.remove_reaction(emoji,user)
-        return
     message = reaction.message
     if message.author.name == bot_name and message.embeds:
+        emoji  = reaction.emoji
+        if emoji != WHITE_HEAVY_CHECK_MARK:
+            await reaction.message.remove_reaction(emoji,user)
+            return
         lobby_title = message.embeds[0].title
         if lobby_title in lobbies and not user.name == bot_name:
             curr_lobby = lobbies[lobby_title]
@@ -23,11 +23,11 @@ async def checkin_player(reaction,user,lobbies):
             await message.edit(embed=curr_lobby.embed())
 
 async def checkout_player(reaction,user,lobbies):
-    emoji = reaction.emoji
-    if emoji != WHITE_HEAVY_CHECK_MARK:
-        return
     message = reaction.message
     if message.author.name == bot_name and message.embeds:
+        emoji = reaction.emoji
+        if emoji != WHITE_HEAVY_CHECK_MARK:
+            return
         lobby_title = message.embeds[0].title
         curr_lobby = lobbies[lobby_title]
         curr_lobby.players.remove(user.name)
