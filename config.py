7cf575d62d7749f1ab10eb479482dade7dc77c3a
phamjames <jamespham2018@gmail.com>
2019-08-27 21:12:28 -0700

    Fixed config file

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ MAX_TITLE_LENGTH = 35
 MAX_DESCRIPTION_LENGTH = 140
 
 # ERR MSGS
-CORRECT_TITLE_FORMAT = "{game}@{hour:minutes}"
+CORRECT_TITLE_FORMAT = "{game}@{hour:minutes}{am/pm}"
 TITLE_FORMAT_INCORRECT = "Incorrect title format. ```correct format: {}```".format(CORRECT_TITLE_FORMAT)
 DESCR_TOO_LONG = "Max description length is " + str(MAX_DESCRIPTION_LENGTH)
 LOBBY_ALREADY_EXISTS = "Lobby already exists."
